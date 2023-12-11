598ed488cfcb5a8cb1deaeba05b93bc51fc98ae4
Vini-boat <85585157+Vini-boat@users.noreply.github.com>
2023-12-11 17:45:55 -0300

    Update Navigation.js

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,7 +4,7 @@ import Auth from './Auth'
 function Navigation(){
     return (
         <Navbar bg="dark" expand='lg'>
-            <Stack direction='horizontal' gap={3}>
+            <Stack direction='horizontal' gap={3} className="w-100 px-2">
                 <Navbar.Brand className='text-white p-2'>Resenhapp</Navbar.Brand>
                 <Auth/>
             </Stack>
